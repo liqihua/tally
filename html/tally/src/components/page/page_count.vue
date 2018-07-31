e6ae74dccb7d3262c05130b961cf356bf5202a5e
liqihua <iamliqihua@qq.com>
2018-07-31 15:09:47 +0800

    liqihua

--- html/tally/src/components/page/page_count.vue
+++ html/tally/src/components/page/page_count.vue
@@ -1,44 +1,123 @@
 <template>
-  <div class="page-count">
-    <div id="main" style="width: 600px;height:400px;"></div>
+  <div class="page-count" v-loading.fullscreen.lock="loading">
+    <div id="chart-bar" style="width: 1000px;height:600px;"></div>
+    <div id="chart-pie" style="width: 1000px;height:600px;"></div>
   </div>
 </template>
 
 <script>
-  import echarts from 'echarts'
+  import API from '@/config/config';
+  import axios from 'axios';
+  import echarts from 'echarts';
 
   export default {
     name : 'count',
+    data() {
+      return {
+        loading : false
+      }
+    },
     mounted(){
       this.drawLine();
     },
     methods :{
       drawLine(){
-        // 基于准备好的dom，初始化echarts实例
-        var myChart = echarts.init(document.getElementById('main'));
+        var userId = localStorage.getItem("userId");
+        var token = localStorage.getItem("token");
+        if(userId == null || userId == '' || token == null || token == ''){
+          location.href = "/login";
+        }
+        var that = this;
+        that.loading = true;
+        axios({
+          method: 'get',
+          url: API.GET_COUNT_TYPE+"?userId="+userId+"&token="+token
+        }).then(function (res) {
+          console.log(res);
+          that.loading = false;
+          if(res.data.resultCode == 10000){
+            if(res.data.resultData != null && res.data.resultData.length > 0){
+              var arrX = [];
+              var arrY = [];
+              var pieArr = [];
+              for(var i=0;i<res.data.resultData.length;i++){
+                var total = res.data.resultData[i].total / 100;
+                arrX.push(res.data.resultData[i].type);
+                arrY.push(total);
+                pieArr.push({name:res.data.resultData[i].type,value:total});
+              }
+              var chartBar = echarts.init(document.getElementById('chart-bar'));
+              var chartPie = echarts.init(document.getElementById('chart-pie'));
+              var optionBar = {
+                title: {
+                  text: '各类型支出总额/元'
+                },
+                tooltip: {},
+                legend: {
+                  data:['支出金额']
+                },
+                xAxis: {
+                  data: arrX
+                },
+                yAxis: {},
+                series: [{
+                  name: '支出金额',
+                  type: 'bar',
+                  data: arrY
+                }]
+              };
 
-        // 指定图表的配置项和数据
-        var option = {
-          title: {
-            text: 'ECharts 入门示例'
-          },
-          tooltip: {},
-          legend: {
-            data:['销量']
-          },
-          xAxis: {
-            data: ["衬衫","羊毛衫","雪纺衫","裤子","高跟鞋","袜子"]
-          },
-          yAxis: {},
-          series: [{
-            name: '销量',
-            type: 'bar',
-            data: [5, 20, 36, 10, 10, 20]
-          }]
-        };
+              var optionPie = {
+                title : {
+                  text: '各类型消费总支出占比',
+                  subtext: '消费比例图',
+                  x:'center'
+                },
+                tooltip : {
+                  trigger: 'item',
+                  formatter: "{a} <br/>{b} : {c} ({d}%)"
+                },
+                legend: {
+                  orient: 'vertical',
+                  left: 'left',
+                  data: arrX
+                },
+                series : [
+                  {
+                    name: '访问来源',
+                    type: 'pie',
+                    radius : '55%',
+                    center: ['50%', '60%'],
+                    data: pieArr,
+                    itemStyle: {
+                      emphasis: {
+                        shadowBlur: 10,
+                        shadowOffsetX: 0,
+                        shadowColor: 'rgba(0, 0, 0, 0.5)'
+                      }
+                    }
+                  }
+                ]
+              };
 
-        // 使用刚指定的配置项和数据显示图表。
-        myChart.setOption(option);
+
+              chartBar.setOption(optionBar);
+              chartPie.setOption(optionPie);
+            }
+
+          }else{
+            if(res.data.resultCode == 42003){
+              location.href = "/login";
+            }
+            that.$message.error({
+              message: res.data.resultMessage,
+              center: true
+            });
+          }
+        }).catch(function (res) {
+          console.log(res);
+          that.loading = false;
+        });
       }
     }
   }
--- html/tally/src/config/config.js
+++ html/tally/src/config/config.js
@@ -1,10 +1,11 @@
-//const API_URL = "http://localhost:9003/tally"
-const API_URL = "http://47.106.100.193"
+const API_URL = "http://localhost:9003/tally"
+//const API_URL = "http://47.106.100.193"
 const API = {
   LOGIN : API_URL + "/api/loginApiController/login",
   SAVE_LOG_PAY : API_URL + "/api/logPayApiController/saveLogPay",
   GET_LOG_LIST : API_URL + "/api/logPayApiController/findPayLogList",
   DELETE_PAY_LOG : API_URL + "/api/logPayApiController/deleteLogPay",
+  GET_COUNT_TYPE : API_URL + "/api/countApiController/countType",
 }
 
 const productType = [
