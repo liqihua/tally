5126e83c6e4f3843cbb394bb0f9760d4d9812da1
liqihua <iamliqihua@qq.com>
2018-08-01 10:24:54 +0800

    liqihua

--- java/tally/src/main/java/com/liqihua/Run.java
+++ java/tally/src/main/java/com/liqihua/Run.java
@@ -3,7 +3,9 @@ package com.liqihua;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @ComponentScan(basePackages={"com.*"})
 @SpringBootApplication
 public class Run {
